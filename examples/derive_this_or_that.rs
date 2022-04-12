bf13508bf184202ac18fbf67efe8c87a86609b82
Michael Baikov <manpacket@gmail.com>
2022-04-12 08:05:17 +0800

    derive this or that example

---
+++ examples/derive_this_or_that.rs
@@ -0,0 +1,25 @@
+#![allow(dead_code)]
+use bpaf::Bpaf;
+
+// By default bpaf tries to parse booleans as flags, do something smart
+// about Strings and file names and handles Option/Vec.
+// Everything else is handled as a textual named argument. In this case
+// we want to use external (with respect to Opts) function `flags`
+// derived for `Flags`. Overall it looks like this:
+
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options)]
+struct Opts {
+    #[bpaf(external(flags))]
+    flag: Flags,
+}
+
+#[derive(Debug, Clone, Bpaf)]
+enum Flags {
+    This,
+    That,
+}
+
+fn main() {
+    println!("{:?}", opts().run())
+}
