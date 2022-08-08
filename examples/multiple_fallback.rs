8f4b42c8fff163980139e95aefa20e55c2129adc
Michael Baikov <manpacket@gmail.com>
2022-08-08 10:22:21 +0800

    multiple_fallback example

---
+++ examples/multiple_fallback.rs
@@ -0,0 +1,47 @@
+//! Fallback to one of several values
+//! - the command line argument
+//! - the environmental variable
+//! - the config file
+//! - the hard-coded default
+
+#![allow(dead_code)]
+use bpaf::*;
+
+#[derive(Clone, Debug)]
+struct Config {
+    field1: u32,
+    field2: u64,
+}
+
+/// Here this is a constant but it can be OnceCell from `once_cell`
+/// that reads the config and deals with substituting missing falues with defaults
+const DEFAULT_CONFIG: Config = Config {
+    field1: 42,
+    field2: 10,
+};
+
+pub fn main() {
+    let field1 = long("field1")
+        .env("FIELD1")
+        .help("Field 1")
+        .argument("ARG")
+        .from_str::<u32>()
+        .fallback(DEFAULT_CONFIG.field1);
+    let field2 = long("field2")
+        .env("FIELD2")
+        .help("Field 2")
+        .argument("ARG")
+        .from_str::<u64>()
+        .fallback(DEFAULT_CONFIG.field2);
+
+    let parser = construct!(Config { field1, field2 });
+
+    let opts = Info::default().for_parser(parser).run();
+
+    // At this point if you get opts - it should be taken from one of
+    // - the command line argument
+    // - the environmental variable
+    // - the config file
+    // - the hard-coded default (from config parser)
+    println!("{:?}", opts);
+}
