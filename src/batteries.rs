b8090320fb8630447f78238cd558ef130e36ae2f
Michael Baikov <manpacket@gmail.com>
2022-08-28 20:52:24 +0800

    batteries: toggle_flag + batteries tests + docs

--- src/batteries.rs
+++ src/batteries.rs
@@ -8,7 +8,7 @@
 //! Examples contain combinatoric usage, for derive usage you should create a parser function and
 //! use `external` annotation.
 
-use crate::{construct, short, Parser};
+use crate::{construct, short, Named, Parser};
 
 /// `--verbose` and `--quiet` flags with results encoded as number
 ///
@@ -87,4 +87,48 @@ pub fn verbose_by_slice<T: Copy + 'static, const N: usize>(
     #![allow(clippy::cast_sign_loss)]
     verbose_and_quiet_by_number(offset as isize, 0, items.len() as isize - 1)
         .map(move |i| items[i as usize])
+}
+
+/// Flag that can be toggled multiple times
+///
+/// Usually `bpaf` only allows to parse a single instance for every invocation unless
+/// [`many`](Parser::many) or [`some`](Parser::some) is used. `toggle_flag` consumes
+/// multiple instances of two different flags and returns last specified value.
+///
+/// This function relies on a fact that selection between two different parsers prefers left most
+/// value. This helps to preserve relative order of parsrs.
+/// You can use similar approach to combine multiple flags accounting for their relative order.
+///
+/// # Example
+/// ```console
+/// $ app --banana --no-banana --banana --banana
+/// Some(Banana)
+/// $ app
+/// None
+/// ```
+///
+/// # Usage
+/// ```rust
+/// # use bpaf::*;
+/// use bpaf::batteries::toggle_flag;
+///
+/// #[derive(Copy, Clone, Debug, PartialEq, Eq)]
+/// enum Select {
+///     Banana,
+///     NoBanana,
+/// }
+///
+/// fn pick() -> impl Parser<Option<Select>> {
+///     toggle_flag(long("banana"), Select::Banana, long("no-banana"), Select::NoBanana)
+/// }
+/// ```
+pub fn toggle_flag<T: Copy + 'static>(
+    a: Named,
+    val_a: T,
+    b: Named,
+    val_b: T,
+) -> impl Parser<Option<T>> {
+    let a = a.req_flag(val_a);
+    let b = b.req_flag(val_b);
+    construct!([a, b]).many().map(|xs| xs.into_iter().last())
 }
