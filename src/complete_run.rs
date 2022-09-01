c3f2a69f1f14f9afb8bc00623049373f9b5d5b83
Michael Baikov <manpacket@gmail.com>
2022-09-01 17:35:45 +0800

    support complete format revisions

--- src/complete_run.rs
+++ src/complete_run.rs
@@ -2,6 +2,10 @@ use std::{ffi::OsString, path::PathBuf};
 
 use crate::{construct, Args};
 
+/// completion variants are dumped as lines of variants by themselves one item on a line
+/// or variants and descriptions separated by a tab
+const REVISION: usize = 1;
+
 #[derive(Clone, Debug, Copy)]
 pub enum Style {
     Bash,
@@ -10,72 +14,106 @@ pub enum Style {
     Elvish,
 }
 
-const BASH_COMPLETER: &str = r#"#/usr/bin/env bash
+fn dump_bash_completer(name: &str) {
+    println!(
+        "\
+#/usr/bin/env bash
 _bpaf_dynamic_completion()
-{
+{{
     COMPREPLY=()
-    IFS='$\n'
-    for line in $( "$1" --bpaf-complete-style-bash "${COMP_WORDS[@]:1}") ; do
-        IFS='$\t' parts=($line)
-        if [[ -n ${parts[1]} ]] ; then
-            COMPREPLY+=($( printf "%-19s %s" "${parts[0]}" "${parts[1]}" ))
+    IFS='$\\n'
+    for line in $( \"$1\" --bpaf-complete-rev={rev} \"${{COMP_WORDS[@]:1}}\") ; do
+        IFS='$\\t' parts=($line)
+        if [[ -n ${{parts[1]}} ]] ; then
+            COMPREPLY+=($( printf \"%-19s %s\" \"${{parts[0]}}\" \"${{parts[1]}}\" ))
         else
-            COMPREPLY+=(${parts[0]})
+            COMPREPLY+=(${{parts[0]}})
         fi
     done
-}"#;
+}}
+complete -F _bpaf_dynamic_completion {name}",
+        name = name,
+        rev = REVISION
+    );
+}
 
-const FISH_COMPLETER: &str = r#"
-function _bpaf_dynamic_completion
-    set -l app (commandline --tokenize --current-process)[1]
-    set -l tmpline --bpaf-complete-style-fish
-    set tmpline $tmpline (commandline --tokenize --current-process)[2..-1]
-    if test (commandline --current-process) != (string trim (commandline --current-process))
-        set tmpline $tmpline ""
-    end
-    for opt in ($app $tmpline)
-        echo -E "$opt"
-    end
-end"#;
+fn dump_zsh_completer(name: &str) {
+    println!(
+        "\
+#compdef {name}
 
-const ZSH_COMPLETER: &str = r#"
 local completions
 local word
 
-IFS=$'\n' completions=($( "${words[1]}" --bpaf-complete-style-zsh "${words[@]:1}" ))
+IFS=$'\\n' completions=($( \"${{words[1]}}\" --bpaf-complete-rev={rev} \"${{words[@]:1}}\" ))
 
 for word in $completions; do
   local -a parts
 
   # Split the line at a tab if there is one.
-  IFS=$'\t' parts=($( echo $word ))
+  IFS=$'\\t' parts=($( echo $word ))
 
   if [[ -n $parts[2] ]]; then
-     if [[ $word[1] == "-" ]]; then
-       local desc=("$parts[1] ($parts[2])")
+     if [[ $word[1] == \"-\" ]]; then
+       local desc=(\"$parts[1] ($parts[2])\")
        compadd -d desc -- $parts[1]
      else
-       local desc=($(print -f  "%-019s -- %s" $parts[1] $parts[2]))
+       local desc=($(print -f  \"%-019s -- %s\" $parts[1] $parts[2]))
        compadd -l -d desc -- $parts[1]
      fi
   else
     compadd -f -- $word
   fi
-done"#;
+done",
+        name = name,
+        rev = REVISION
+    );
+}
 
-const ELVISH_COMPLETER: &str = r#"use str;
-     for line $lines {
-         var @arg = (str:split "\t" $line)
-         try {
-             edit:complex-candidate $arg[0] &display=( printf "%-19s %s" $arg[0] $arg[1] )
-         } catch {
+fn dump_fish_completer(name: &str) {
+    println!(
+        "\
+function _bpaf_dynamic_completion
+    set -l app (commandline --tokenize --current-process)[1]
+    set -l tmpline --bpaf-complete-rev={rev}
+    set tmpline $tmpline (commandline --tokenize --current-process)[2..-1]
+    if test (commandline --current-process) != (string trim (commandline --current-process))
+        set tmpline $tmpline \"\"
+    end
+    for opt in ($app $tmpline)
+        echo -E \"$opt\"
+    end
+end
+
+complete --no-files --command {} --arguments '(_bpaf_dynamic_completion)'",
+        name = name,
+        rev = REVISION
+    );
+}
+
+fn dump_elvish_completer(name: &str) {
+    println!(
+        "\
+set edit:completion:arg-completer[{name}] = {{ |@args| var args = $args[1..];
+     var @lines = ( {name} --bpaf-complete-rev={rev} $@args );
+     use str;
+     for line $lines {{
+         var @arg = (str:split \"\\t\" $line)
+         try {{
+             edit:complex-candidate $arg[0] &display=( printf \"%-19s %s\" $arg[0] $arg[1] )
+         }} catch {{
              edit:complex-candidate $line
-         }
-     }
-}"#;
+         }}
+     }}
+}}",
+        name = name,
+        rev = REVISION
+    );
+}
 
-struct CompOptions {
-    style: Style,
+enum CompOptions {
+    Dump { style: Style },
+    Complete { revision: usize },
 }
 
 fn parse_comp_options() -> crate::OptionParser<CompOptions> {
@@ -85,7 +123,14 @@ fn parse_comp_options() -> crate::OptionParser<CompOptions> {
     let fish = long("bpaf-complete-style-fish").req_flag(Style::Fish);
     let elvish = long("bpaf-complete-style-elvish").req_flag(Style::Elvish);
     let style = construct!([zsh, bash, fish, elvish]);
-    construct!(CompOptions { style }).to_options()
+    let dump = construct!(CompOptions::Dump { style });
+
+    let revision = long("bpaf-complete-rev")
+        .argument("REV")
+        .from_str::<usize>();
+    let complete = construct!(CompOptions::Complete { revision });
+
+    construct!([complete, dump]).to_options()
 }
 
 pub(crate) fn args_with_complete(
@@ -105,34 +150,28 @@ pub(crate) fn args_with_complete(
 
     match parse_comp_options().run_inner(cargs) {
         Ok(comp) => {
-            if arguments.is_empty() {
-                let name = match path {
-                    Some(path) => path,
-                    None => panic!("app name is not utf8, giving up rendering completer"),
-                };
-                // render prefered completer
-                match comp.style {
-                    Style::Bash => println!(
-                        "{}\ncomplete -F _bpaf_dynamic_completion {}",
-                        BASH_COMPLETER, name
-                    ),
-                    Style::Zsh => println!("#compdef {}\n{}", name, ZSH_COMPLETER),
-                    Style::Fish => println!(
-                        "{}
-complete --no-files --command {} --arguments '(_bpaf_dynamic_completion)'",
-                        FISH_COMPLETER, name
-                    ),
-                    Style::Elvish => println!(
-                        "set edit:completion:arg-completer[{}] = {{ |@args| var args = $args[1..];
-     var @lines = ( {} --bpaf-complete-style-elvish $@args );
-{}",
-                        name, name, ELVISH_COMPLETER
-                    ),
-                };
-                std::process::exit(0)
-            } else {
-                Args::from(arguments).set_comp()
+            let name = match path {
+                Some(path) => path,
+                None => panic!("app name is not utf8, giving up rendering completer"),
+            };
+
+            match comp {
+                CompOptions::Dump { style } => {
+                    match style {
+                        Style::Bash => dump_bash_completer(name),
+                        Style::Zsh => dump_zsh_completer(name),
+                        Style::Fish => dump_fish_completer(name),
+                        Style::Elvish => dump_elvish_completer(name),
+                    }
+                    std::process::exit(0)
+                }
+                CompOptions::Complete { revision } => {
+                    if revision != REVISION {
+                        panic!("Revision mismatch, expected {}, got {}. You should regenerate completion files for your shell", REVISION, revision);
+                    }
+                }
             }
+            Args::from(arguments).set_comp()
         }
 
         Err(err) => {
