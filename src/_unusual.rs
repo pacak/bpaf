a2e8ab392fb3cdbb7759a1a3a0a16c2b4ef62df3
Michael Baikov <manpacket@gmail.com>
2022-09-21 12:13:17 -0400

    unusual example xorg

--- src/_unusual.rs
+++ src/_unusual.rs
@@ -1,7 +1,7 @@
 //! # Some of the more unusual examples
 //!
-//! While `bpaf` is designed with some common use cases in mind - mostly posix conventions
-//! <https://pubs.opengroup.org/onlinepubs/9699919799.2018edition/basedefs/V1_chap12.html>
+//! While `bpaf` is designed with some common use cases in mind - mostly
+//! [posix conventions](https://pubs.opengroup.org/onlinepubs/9699919799.2018edition/basedefs/V1_chap12.html)
 //! it can also handle some more unusual requirements. It might come at a cost of having to write
 //! more code, more confusing error messages or worse performance, but it will get the job done.
 
@@ -14,3 +14,8 @@ pub mod find {}
 ///
 #[doc = include_str!("docs/dd.md")]
 pub mod dd {}
+
+/// ## Xorg(1): `Xorg +xinerama +extension name`
+///
+#[doc = include_str!("docs/xorg.md")]
+pub mod xorg {}
