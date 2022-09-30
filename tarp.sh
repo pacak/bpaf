25eefcbac810a09e618a38dd33b24e331647e8ab
Michael Baikov <manpacket@gmail.com>
2022-09-30 08:33:21 -0400

    improve coverage tests

--- tarp.sh
+++ tarp.sh
@@ -1,1 +1,10 @@
-cargo tarpaulin --target-dir tarp -p bpaf -p docs --exclude-files tests/**/*.rs --exclude-files docs/**/*.rs
+cargo tarpaulin --target-dir tarp \
+    --workspace \
+    --all-features \
+    --exclude-files tests/*.rs \
+    --exclude-files docs/*/*.rs \
+    --exclude-files docs/*/*/*.rs \
+    --exclude-files docs/build.rs \
+    --exclude-files bpaf_cauwugo/src/*.rs \
+    --exclude-files bpaf_derive/src/*_tests.rs \
+    --exclude-files src/tests.rs
