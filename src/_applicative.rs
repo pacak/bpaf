9c251f24576b13ca786ea2d7541a7e0eb5fc9714
Michael Baikov <manpacket@gmail.com>
2022-10-04 13:57:28 -0400

    Add more info about Applicative Functors

--- src/_applicative.rs
+++ src/_applicative.rs
@@ -98,14 +98,56 @@
 //! but `Applicative Functors` in general don't have to - in fact to implement dynamic completion
 //! `bpaf` needs to check items past the first failure point to collect all the possible
 //! completions.
-//!
 
-//! ## `Parser` trait
+//! ## Alternative Functors
 //!
-//! Similarly to `Reader` struct described above individual parsers don't actually contain a value
-//! but only a value in context - they contain a computation describing the initial consumer plus
+//! So far `Applicative Functors` allow us to create structs containing multuple fields out of
+//! individual parsers for each field. `Alternative` typeclass in Haskell extends `Applicative`
+//! with two extra operations: `empty` and `<|>`, later allows to choose a better element out of
+//! two alternatives and the former is an identity element for this choosing operation. In Rust
+//! a closest analogy would be `Option::None` and `Option::or`:
+//!
+//! ```rust
+//! fn pick_number(a: Option<u32>, b: Option<u32>) -> Option<u32> {
+//!     a.or(b)
+//! }
+//!
+//! let present_1 = Some(10);
+//! let present_2 = Some(20);
+//! let empty = None;
+//! assert_eq!(pick_number(present_1, present_2), present_1);
+//! assert_eq!(pick_number(present_1, empty), present_1);
+//! assert_eq!(pick_number(empty, present_1), present_1);
+//! assert_eq!(pick_number(empty, empty), empty);
+//! ```
+
+//! ## `Parser` trait and `construct!` macro
+//!
+//! Similarly to `Reader` struct described above individual parsers don't actually contain any values
+//! but only define a context for them - they contain a computation describing the initial consumer plus
 //! all the transformations and validations layered on top. [`construct!`] macro takes several
-//! parser objects and composes them similarly to `add_numbers` function above.
+//! parser objects and composes them either according to `Applicative` or `Alternative` functor
+//! laws depending on the usage.
+
+//! ## So why use `Applicative Functors` then?
+//!
+//! As a user I want to be able to express requirements using full power of Rust algebraic
+//! datatypes: `struct` for product types and `enum` for sum types. To give an example -
+//! `cargo-show-asm` asks user to specify what to output - Intel or AT&T asm, LLVM or Rust's MIR
+//! and opts to represent it as one of four flags: `--intel`, `--att`, `--llvm` and `--mir`. While
+//! each flag can be though of a boolean value - present/absent - consuming it as an enum with four
+//! possible values is much more convenient compared to tuple with all the possible combinations.
+//! `Applicative` interface gives just enough power to compose simple parsers as an arbitrary tree
+//! ready for consumption.
+//!
+//! As a library author I need to be able to extract information from the tree constructed by user
+//! to generate `--help` information and do command line completion. As long as the tree only uses
+//! `Applicative` powers - it is possible to evaluate it without giving any input, while
+//! adding `Monadic` powers (deciding what to parse next depending on the previous input) would
+//! make this impossible.
+//!
+//! So `Applicative Functors` sit right in the middle between what users want to express and
+//! library can consume.
 
 #[cfg(doc)]
-use crate::construct;
+use crate::*;
