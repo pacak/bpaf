c62abd9d3be39f5916e3944ec5d25e79b9feb279
Michael Baikov <manpacket@gmail.com>
2022-10-05 11:38:18 -0400

    more applicative functors intro

--- src/_applicative.rs
+++ src/_applicative.rs
@@ -5,15 +5,11 @@
 
 //! ## Functors
 //!
-//! Let's start by talking about what a `Functor` is. In Haskell it is defined as
-//!
-//! ```haskell
-//! class Functor f where
-//!     fmap :: (a -> b) -> f a -> f b
-//! ```
-//!
-//! looks scary, but in Rust terms it's a trait that takes a *value in a context* or a container such as
-//! `Option<A>` and a function `fn(A) -> B` and gives you `Option<B>` back.
+//! Let's start by talking about what a `Functor` is. Wikipedia defines it as a "design pattern
+//! that allows for a generic type to apply a function inside without changing the structure of
+//! the generic type". Sounds scary, but in Rust terms it's a trait that takes a value or values
+//! in a container (or more general *value in a context* ) such as `Option<A>` and a function
+//! `fn(A) -> B` and gives you `Option<B>` back.
 //!
 //! Closest analogy in a real code you can write in Rust right now would be modifying an `Option`
 //! using only `Option::map`:
@@ -29,19 +25,19 @@
 //! assert_eq!(plus_one(absent), None);
 //! ```
 //!
-//! `Vec`, `Result` and other types that implement `map` are Functors as well, but functors are not
-//! limited just to containers - you don't have to have a value inside to be able to manipulate it.
-//! Consider `Reader` that allows you to perform pure transformations on a *value in a context* `T`
-//! without having any value until it's ready to execute:
+//! `Vec`, `Result` and other types that implement `map` are `Functor` as well, but `Functor`
+//! is not limited just to containers - you don't have to have a value inside to be able to
+//! manipulate it. In fact a regular rust function is also a `Functor` if you squint hard enough:
+//! consider `Reader` that allows you to perform transformations on a *value in a context* `T`
+//! without having any value until it the execution time:
 //!
 //! ```rust
-//! # use std::marker::PhantomData;
-//! struct Reader<T>(PhantomData<T>, Box<dyn Fn(T) -> T>);
+//! struct Reader<T>(Box<dyn Fn(T) -> T>);
 //!
 //! impl<T> Reader<T> {
 //!     /// Initialize an new value in a context
 //!     fn new() -> Self {
-//!         Self(PhantomData, Box::new(|x| x))
+//!         Self(Box::new(|x| x))
 //!     }
 //!
 //!     /// Modify a value in a context
@@ -51,9 +47,8 @@
 //!         T: 'static,
 //!     {
 //!         Self(
-//!             PhantomData,
 //!             Box::new(move |x| {
-//!                 let inner = &self.1;
+//!                 let inner = &self.0;
 //!                 f(inner(x))
 //!             }),
 //!         )
@@ -61,7 +56,7 @@
 //!
 //!     /// Apply the changes by giving it the initial value
 //!     fn run(self, input: T) -> T {
-//!         let f = &self.1;
+//!         let f = &self.0;
 //!         f(input)
 //!     }
 //! }
@@ -74,9 +69,10 @@
 
 //! ## Applicative Functors
 //!
-//! `map` in `Functors` is limited to a single value, `Applicative Functors` extend it to multiple values, closest Rust
-//! analogy would be doing computations on `Option` or `Result` using only `?` and `Some`/`Ok` but
-//! without making any decisions on input values after they been extracted from `Option`.
+//! `map` in `Functor` is limited to a single *value in a context*, `Applicativ Functor` extends it
+//! to operations combining multiple values, closest Rust analogy would be doing computations on
+//! `Option` or `Result` using only `?` and `Some`/`Ok` but without making any decisions that would
+//! change the shape of the output based on input values after they been extracted with `?`.
 //! ```rust
 //! fn add_numbers(input_a: Option<u32>, input_b: Option<u32>) -> Option<u32> {
 //!     Some(input_a? + input_b?)
@@ -101,11 +97,10 @@
 
 //! ## Alternative Functors
 //!
-//! So far `Applicative Functors` allow us to create structs containing multuple fields out of
-//! individual parsers for each field. `Alternative` typeclass in Haskell extends `Applicative`
-//! with two extra operations: `empty` and `<|>`, later allows to choose a better element out of
-//! two alternatives and the former is an identity element for this choosing operation. In Rust
-//! a closest analogy would be `Option::None` and `Option::or`:
+//! So far `Applicative Functors` allow us to create structs containing multiple fields out of
+//! individual parsers for each field. `Alternative` extends `Applicative` with two extra
+//! things: one for choosing a better of two *values in a context* and an idenity element
+//! for this operation. In Rust a closest analogy would be `Option::or` and `Option::None`:
 //!
 //! ```rust
 //! fn pick_number(a: Option<u32>, b: Option<u32>) -> Option<u32> {
@@ -123,7 +118,7 @@
 
 //! ## `Parser` trait and `construct!` macro
 //!
-//! Similarly to `Reader` struct described above individual parsers don't actually contain any values
+//! Similarly to `Reader` struct described above parsers don't actually contain any values
 //! but only define a context for them - they contain a computation describing the initial consumer plus
 //! all the transformations and validations layered on top. [`construct!`] macro takes several
 //! parser objects and composes them either according to `Applicative` or `Alternative` functor
@@ -141,12 +136,12 @@
 //! ready for consumption.
 //!
 //! As a library author I need to be able to extract information from the tree constructed by user
-//! to generate `--help` information and do command line completion. As long as the tree only uses
-//! `Applicative` powers - it is possible to evaluate it without giving any input, while
-//! adding `Monadic` powers (deciding what to parse next depending on the previous input) would
+//! to generate `--help` information and do command line completion. As long as the tree uses only
+//! `Applicative` powers - it is possible to evaluate it without giving it any input.
+//! Adding `Monadic` powers (deciding what to parse next depending on the previous input) would
 //! make this impossible.
 //!
-//! So `Applicative Functors` sit right in the middle between what users want to express and
+//! So `Applicative Functors` sits right in the middle between what users want to express and
 //! library can consume.
 
 #[cfg(doc)]
