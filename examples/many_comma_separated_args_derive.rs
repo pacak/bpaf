b801b323889dfebd33cb4d7828c36042a273f7a3
Michael Baikov <manpacket@gmail.com>
2023-02-19 09:15:48 -0500

    examples: many comma separated arguments

---
+++ examples/many_comma_separated_args_derive.rs
@@ -0,0 +1,30 @@
+use bpaf::*;
+use std::{num::ParseIntError, str::FromStr};
+
+fn split_and_parse(s: String) -> Result<Vec<u16>, ParseIntError> {
+    s.split(",")
+        .map(|c| u16::from_str(c))
+        .collect::<Result<Vec<_>, _>>()
+}
+
+fn flatten_vec(vv: Vec<Vec<u16>>) -> Vec<u16> {
+    vv.into_iter().flatten().collect()
+}
+
+#[derive(Debug, Clone, Bpaf)]
+#[allow(dead_code)]
+struct Opts {
+    #[bpaf(
+        long,
+        argument::<String>("PORTS"),
+        parse(split_and_parse),
+        many,
+        map(flatten_vec)
+    )]
+    /// Comma separated list of ports
+    ports: Vec<u16>,
+}
+
+fn main() {
+    println!("{:?}", opts().to_options().run());
+}
