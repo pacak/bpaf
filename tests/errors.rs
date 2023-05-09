55a750ba2c5368c29ec3d1608874e5a48459d5ad
Michael Baikov <manpacket@gmail.com>
2023-05-09 07:20:44 -0400

    Add a test with a bad error message

--- tests/errors.rs
+++ tests/errors.rs
@@ -49,3 +49,23 @@ fn guard_message() {
 
     assert_eq!(res, "\"30\": too high");
 }
+
+#[test]
+fn cannot_be_used_multiple_times() {
+    let parser = short('a').switch().to_options();
+
+    let r = parser
+        .run_inner(Args::from(&["-aaa"]))
+        .unwrap_err()
+        .unwrap_stderr();
+    let expected = "-a is not expected in this context";
+    assert_eq!(r, expected);
+
+    // TODO - improve error message
+    let r = parser
+        .run_inner(Args::from(&["-a", "-a"]))
+        .unwrap_err()
+        .unwrap_stderr();
+    let expected = "-a is not expected in this context";
+    assert_eq!(r, expected);
+}
