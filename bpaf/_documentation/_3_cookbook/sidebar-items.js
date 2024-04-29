cc4883b21d486453fc80b56d89e5919f92c3d17d
GitHub Action <action@github.com>
2024-04-29 12:09:37 +0000

    deploy

---
+++ bpaf/_documentation/_3_cookbook/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["_00_find","_01_dd","_02_xorg","_03_command_chaining","_04_multi_value","_05_struct_groups","_06_multi_flag","_07_skip_positional","_08_cargo_helper","_09_numeric_flags"]};
