0ce96d00ad9a90adfbb5112f7ba4b0ce09895a2f
Michael Baikov <manpacket@gmail.com>
2024-07-27 13:23:30 -0400

    Add a test for non_strict completion
    it should not suggest adding double dash

--- tests/completion.rs
+++ tests/completion.rs
@@ -937,6 +937,25 @@ fn should_be_able_to_suggest_double_dash() {
 }
 
 #[test]
+fn dont_suggest_double_dash_automatically_for_nonstrictly_positional() {
+    let a = short('a').switch();
+    let b = positional::<String>("B").non_strict();
+    let parser = construct!(a, b).to_options();
+
+    let r = parser
+        .run_inner(Args::from(&[""]).set_comp(0))
+        .unwrap_err()
+        .unwrap_stdout();
+
+    assert_eq!(
+        r,
+        "\
+-a\t-a\t\t
+\tB\t\t\n\n"
+    );
+}
+
+#[test]
 fn suggest_double_dash_automatically_for_strictly_positional() {
     let a = short('a').switch();
     let b = positional::<String>("B").strict();
