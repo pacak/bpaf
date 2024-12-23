23f94d8d7857d29ed8a242fb3de06a3d0aa7dd88
Michael Baikov <manpacket@gmail.com>
2024-12-22 20:07:35 -0500

    microadvance

--- bpaf_core/tests/args.rs
+++ bpaf_core/tests/args.rs
@@ -21,3 +21,13 @@ fn short_ajoint() {
     let r = run_parser(&parser(), &["-a10"]);
     assert_eq!(r, Ok(10));
 }
+
+#[test]
+fn merged_shorts_simple() {
+    let a = short('a').switch();
+    let b = short('b').switch();
+    let c = short('c').switch();
+    let parser = construct!(a, b, c);
+    let r = run_parser(&parser, ["-abc"]).unwrap();
+    assert_eq!(r, (true, true, true));
+}
