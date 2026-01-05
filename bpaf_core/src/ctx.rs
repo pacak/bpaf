007657cc038d15921c1adf998fd53dc9432722a3
Mikhail Baykov <manpacket@gmail.com>
2026-01-05 04:18:09 -0500

    comments, shuffling things

--- bpaf_core/src/ctx.rs
+++ bpaf_core/src/ctx.rs
@@ -1,8 +1,14 @@
+//! A context shared between [`Task`] and [`Executor`]. Ideally I want to pass it to tasks when I'm
+//! trying to run them, but async monad is very limited in Rust so shared state in [`Rc`] it is.
+
 use std::{
     cell::{Cell, RefCell},
     collections::{BTreeSet, VecDeque},
     rc::Rc,
 };
+
+#[allow(unused)] // used by `rustdoc`,
+use crate::Executor;
 
 use crate::{
     Conflict, Id, KillReason, Parent, Reason, TChange, TTarget, Task, TaskInfo, args::Args,
@@ -13,17 +19,22 @@ pub struct RawCtx {
     ///
     /// Holds other operations that might need access to tasks or other internal structures
     pub(crate) pending_ops: RefCell<VecDeque<Op>>,
+
     /// Early exit ranges
     ///
     /// When there's no matching triggers executor will try to terminate anything inside of
     /// the each pair. This allows is necessary for things like `.optional()` and `.many()` to work
     pub(crate) early_exit: RefCell<BTreeSet<Scope>>,
+
     /// Next free Id
     ///
     /// Tasks can use it to allocate `Id`s for children tasks including overriding
     pub(crate) next_free: Cell<u32>,
+
+    /// Arguments we are parsing as well as a cursor
     pub(crate) args: Args,
     pub(crate) cursor: Cell<usize>,
+
     /// When task is woken up this contains a reason for it
     pub(crate) wakeup_reason: RefCell<Reason>,
     /// Reference to [`TaskInfo`] for the current task
